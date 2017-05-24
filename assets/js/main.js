1bb8a39e44ff71bd6d596dc3ea6dbc9ffa48d14f
anisen <ananavelez@gmail.com>
2017-05-24 10:55:46 -0400

    Lo que se alcanzo a hacer

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,31 @@ numero.addEventListener("click", function(){
   if(num.length!=9){
     alert("Ingrese numero de 9 digitos");
   }else{
-    alert("Su número se ha registrado correctamente");
+    alert("A sido registrado correctamente");
   }
 });
+/*validacion de coordenadas*/
 
+var start=document.getElementById("botonauto");
+
+start.addEventListener('click', function(){
+  var x = document.getElementById("x-auto").value;
+  var y = document.getElementById("y-auto").value;
+  x=parseInt(x);
+  y=parseInt(y);
+
+  if (isNaN(x)==true||isNaN(y)==true){
+    alert("Ingresar números");
+  }
+  else if(x>10){
+    alert ("Para X (número entre el 1 al 10)");
+  }else if(y>6){
+    alert ("Para Y (número entre 1 al 6)");
+  }else{
+    
+    return true;
+  }
+});
 
 /* tablero*/
 var teclas = {
@@ -47,20 +68,20 @@ function dibujarTeclado(evento)
   switch(evento.keyCode)
   {
     case teclas.UP:
-      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
-      y = y - movimiento;
+    dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+    y = y - movimiento;
     break;
     case teclas.DOWN:
-      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
-      y = y + movimiento;
+    dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+    y = y + movimiento;
     break;
     case teclas.LEFT:
-      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
-      x = x - movimiento;
+    dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+    x = x - movimiento;
     break;
     case teclas.RIGHT:
-      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
-      x = x + movimiento;
+    dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+    x = x + movimiento;
     break;
   }
 }
